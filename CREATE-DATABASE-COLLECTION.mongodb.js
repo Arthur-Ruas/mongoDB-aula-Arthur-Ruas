2556a6f486e5a7e6beba0dc026477211ac3c109e
Arthur-Ruas <arthurruasfe@gmail.com>
2024-04-30 20:32:52 -0300

    Arthur: Inicio do projeto

---
+++ CREATE-DATABASE-COLLECTION.mongodb.js
@@ -0,0 +1,11 @@
+/* Define o nome do banco de dados */
+const database = 'libri-database';
+
+/* Define o nome da collection */
+const collection = 'libri-collection';
+
+/* Acessa o banco de dados livri-database */
+use(database);
+
+/* Cria a collection libr-collection */
+db.createCollection(collection)
